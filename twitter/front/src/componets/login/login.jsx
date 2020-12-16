c7c96b5952c2fb250d2b005a728939e725cb65d0
6691a <6691a@naver.com>
2020-12-16 17:53:51 +0900

    login page 거의 완료

--- twitter/front/src/componets/login/login.jsx
+++ twitter/front/src/componets/login/login.jsx
@@ -4,36 +4,39 @@ import styles from './login.module.css';
 const Login = (props) => {
     const emailRef = useRef();
     const passwordRef = useRef();
-    // const email_label = useRef();
-    
-    // const onFocus = (current) => {
-    //     ReactDOM.findDOMNode(email_label.current).style.border ="0.1rem solid #1da1f2";
-    // }
-
-    // const onBlur = (current) => {
-    //     console.log(current);
-    //     ReactDOM.findDOMNode(email_label.current).style.border ="border: 0.1rem solid #c4cfd6";
-    // }
-
 
     return(
     <div className={styles.login}>
         <img className={styles.logo} src="/images/twitter.svg" alt="twitter_logo"/>
         <h1 className={styles.text}>트위터 로그인</h1>
         <div className={styles.input_field}>
-            <label  className={styles.input_label_email}>
-                <span className={styles.input_text}>이메일</span>
-                    <input ref={emailRef} type="text"/> 
+            <form action="">
+                <label className={styles.input_label_email}>
+                    <span className={styles.input_text}>이메일</span>
+                    <input className ={styles.email_input}ref={emailRef} type="text"/> 
                 </label>
-            <label className={styles.input_label_password}>
-                <span className={styles.input_text}>비밀번호</span>
-                <input  ref={passwordRef} type="password" />
-            </label>
-            <button type="submit">로그인</button>
-            <div>
-                <span>비밀번호를 잊으셨나요?</span>
-                <span>.</span>
-                <span>트위터 가입</span>
+                <label className={styles.input_label_password}>
+                    <span className={styles.input_text}>비밀번호</span>
+                    <input  ref={passwordRef} type="password"/>
+                </label>
+
+                <button className={styles.submit_Btn} type="submit"
+                disabled ={
+                false
+                // emailRef.current &&
+                // passwordRef.currnet
+                // ? false
+                // : true
+                }
+                >로그인</button>
+            </form>
+                
+
+            
+            <div className={styles.info}>
+                <span className={styles.info_text}>비밀번호를 잊으셨나요?</span>
+                <span className={styles.info_text_space}>·</span>
+                <span className={styles.info_text}>트위터 가입</span>
             </div>
         </div>
     </div>
